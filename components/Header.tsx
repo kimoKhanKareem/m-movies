6146386acc4d8a11bbfa08b338cc0b64107e3a77
Karim Adel <87901216+kimoKhanKareem@users.noreply.github.com>
2022-06-25 15:32:39 +0200

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,35 @@
 // import Image from 'next/image';
 import styled from 'styled-components'
-import {  BsSearch  } from 'react-icons/bs';
+import { BsSearch } from 'react-icons/bs';
+import { AiFillBell } from 'react-icons/ai';
+import Link from 'next/link';
+
 
 
 function Header() {
     return (
         <NavBar>
-            <div>
-                <img src="https://rb.gy/ulxxee" width={200} height={100}  alt="logo"/>
+            <div className="rigth-nav">
+                <div>
+                    <img src="https://rb.gy/ulxxee" width={200} height={100} alt="logo" />
+                </div>
+
+                <ul>
+                    <li>Home</li>
+                    <li>Tv Shows</li>
+                    <li>Movies</li>
+                    <li>New & Popular</li>
+                    <li>My List</li>
+                </ul>
+
             </div>
 
-            <ul>
-                <li>Home</li>
-                <li>Tv Shows</li>
-                <li>Movies</li>
-                <li>New & Popular</li>
-                <li>My List</li>
-            </ul>
-
             <div className='icons'>
-                <BsSearch />
+                <BsSearch  style={{fontSize:'1.5rem'}} />
+                <AiFillBell  style={{marginLeft: '10px', fontSize:'2rem'}} />
+                <Link href="/account">
+                    <img src="https://rb.gy/g1pwyx" alt="..." width={35} height={35}  style={{marginLeft: '10px'}} />
+                </Link>
             </div>
         </NavBar>
     )
@@ -27,21 +37,25 @@ function Header() {
 
 //  styled-components
 const NavBar = styled.header`
-width: 100%;
 display: flex;
 align-items: center;
-ul{
+.rigth-nav{
     display: flex;
-    li{
-        margin-left: 2rem;
+align-items: center;
+    ul{
+        display: flex;
+        li{
+            margin-left: 2rem;
+            cursor: pointer;
+            font-size: 1.5rem;
+        }
     }
 }
 .icons{
     display: flex;
     justify-content: center;
+    align-items: center;
+    cursor: pointer;
 }
-
-
-
 `;
 export default Header
